f46dfa54088629e97b97121b10145d0068faecc6
carpenter <team@carpentries.org>
2023-05-15 11:59:05 +0300

    1st complete draft the episode 1

---
+++ create_and_build.R
@@ -0,0 +1,18 @@
+
+# Build lesson
+sandpaper::serve()
+
+
+
+#### Create new lesson
+# library("fs") # file system package for cross-platform paths
+# library("sandpaper")
+#
+# # Create a brand new lesson on your desktop called "buoyant-barnacle"
+# bb <- path_home("Desktop/buoyant-barnacle")
+# print(bb) # print the new path to your screen
+# create_lesson(bb) # create a new lesson in that path
+
+
+#### Create episode
+# sandpaper::create_episode("Setup")
